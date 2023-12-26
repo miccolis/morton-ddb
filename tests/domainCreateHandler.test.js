519a11c219fd1fc3e203e1531ee4e9fac37e782f
Jeff Miccolis <jeff@miccolis.net>
2023-12-25 19:39:11 -0500

    Validate domain id

---
+++ tests/domainCreateHandler.test.js
@@ -0,0 +1,28 @@
+import t from "tap";
+
+import { validateDomain } from "../lib/domainCreateHandler.js";
+
+t.test("validateDomain", (t) => {
+  try {
+    validateDomain({ name: "test", access: "public", ttl: 123 });
+    t.ok(true);
+  } catch (e) {
+    t.ok(false);
+  }
+
+  try {
+    validateDomain({ name: "test", access: "foo", ttl: 123 });
+    t.ok(false);
+  } catch (e) {
+    t.equal(e.statusCode, 400);
+  }
+
+  try {
+    validateDomain({ name: "test", access: "public", ttl: "123" });
+    t.ok(false);
+  } catch (e) {
+    t.equal(e.statusCode, 400);
+  }
+
+  t.end();
+});
